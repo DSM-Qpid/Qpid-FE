1305ebff7c56976f75f40858c81b8fefc9fff3bc
smp6340 <smp6340@naver.com>
2023-07-04 21:37:50 +0900

    Publishing :: 퍼블리싱 완료

---
+++ src/pages/search/style.ts
@@ -0,0 +1,131 @@
+import styled from "styled-components";
+import "../../asset/font/font.css";
+
+export const View = styled.div`
+    width: 360px;
+    height: 536px;
+`;
+
+export const SearchContainer = styled.div`
+    width: 100%;
+    height: 205px;
+    background-color: #6A5DFF;
+    border-radius: 0 0 30px 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export const TitleContainer = styled.div`
+    width: auto;
+    height: auto;
+    margin-left: 16px;
+`;
+
+export const SubTitle = styled.div`
+    width: auto;
+    height: auto;
+    font-family: "Pretendard_R";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #fff;
+    margin-left: 10px;
+`;
+
+export const MainTitle = styled.div`
+    width: 210px;
+    height: auto;
+    font-family: "Pretendard_B";
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    color: #fff;
+    margin-left: 10px;
+`;
+
+export const Img = styled.img`
+    width: auto;
+    height: auto;
+    margin-left: 10px;
+`;  
+
+export const SearchBar = styled.div`
+    width: 328px;
+    height: 56px;
+    border: 1px solid #564BD4;
+    border-radius: 100px;
+    font-family: "Pretendard_M";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    color: #D0D0D0;
+    background-color: #fff;
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const SeachImg = styled.img`
+    width: auto;
+    height: auto;
+    margin-left: 44px;
+`;
+
+export const SearchWordContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+export const SearchWordWrapper = styled.div`
+    width: 328px;
+    height: auto;
+`;
+
+export const Title = styled.div`
+    font-family: "Pretendard_M";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #000;
+    margin-top: 10px;
+    margin-left: 10px;
+    margin-bottom: 10px;
+`;
+
+export const RecentContainer = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+`;
+
+export const Recent = styled.div`
+    width: auto;
+    height: 31px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #6A5DFF;
+    border-radius: 100px;
+    margin-right: 8px;
+`;
+
+export const Word = styled.div`
+    font-family: "Pretendard_M";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #fff;
+    margin: 6px 0 6px 16px;
+`;
+
+export const CancelImg = styled.img`
+    width: 12px;
+    height: 12px;
+    margin-left: 4px;
+    margin-right: 12px;
+`;
